228b5cf8255b226f292c13c9c774030fb55d8cd4
benp <ben@ioxu.com>
2014-07-29 00:03:31 +0930

    added distance comparison

--- utils.py
+++ utils.py
@@ -5,4 +5,9 @@ def distance(x1, y1, x2, y2):
 	l = (x2 - x1) * (x2 - x1 ) + (y2 - y1) * (y2 - y1)
 	return math.sqrt(l)
 
+def distance2(x1, y1, x2, y2):
+	"""calc distance between (x1,y1) and (x2, y2)
+	without sqrt of result - faster for length comparisons"""
+	l = (x2 - x1) * (x2 - x1 ) + (y2 - y1) * (y2 - y1)
+	return l
 
